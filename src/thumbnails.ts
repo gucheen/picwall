16ad11e341c5966681256c1e1c46e6c9d9558f63
gucheen <guchengf@gmail.com>
2025-06-06 10:26:06 +0800

    feat: compress photos

--- src/thumbnails.ts
+++ src/thumbnails.ts
@@ -1,18 +1,30 @@
 import { readdir } from 'node:fs/promises'
 import sharp from 'sharp'
 
-const photoFiles = await readdir('./photos')
+const photoFiles = await readdir('./raw')
 
 await Promise.all(photoFiles.map((fileName) => {
-  const image = sharp(`./photos/${fileName}`)
-  .resize({
-    width: 1000,
-    height: 1000,
-    fit: 'inside',
-  })
-  .rotate()
+  const image = sharp(`./raw/${fileName}`)
+    .rotate()
   return [
     image.clone()
-    .toFile(`./thumbnails/${fileName}.avif`),
+      .jpeg({
+        mozjpeg: true,
+      })
+      .toFile(`./photos/${fileName}`),
+  ]
+}).flat())
+
+await Promise.all(photoFiles.map((fileName) => {
+  const image = sharp(`./raw/${fileName}`)
+    .resize({
+      width: 1000,
+      height: 1000,
+      fit: 'inside',
+    })
+    .rotate()
+  return [
+    image.clone()
+      .toFile(`./thumbnails/${fileName}.avif`),
   ]
 }).flat())
